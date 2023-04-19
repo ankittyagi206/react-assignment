190b51198532b292d0e3da176c969b4b598ca58c
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2023-04-19 14:56:04 +0530

    fix: problempage

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link, useRouteError } from "react-router-dom";
 
 const Error = () => {
-  return <div>Opps!</div>;
+  const { status, statusText } = useRouteError();
+
+  return (
+    <div>
+      <h1> {`Opps! ${status} ${statusText}`}</h1>
+
+      <Link to="/">
+        <a>Click here to go back to Home Page</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
